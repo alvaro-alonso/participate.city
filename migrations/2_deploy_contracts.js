3dc0dbfeb8fec0dfd6f95b6d51ace058bf8bf3e5
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-01-29 10:28:33 +0100

    Voting deployment fixed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,13 @@ const Pairing = artifacts.require('Pairing');
 const Verifier = artifacts.require('Verifier');
 const Register = artifacts.require('ElectionRegistry');
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   deployer.deploy(BN256G2);
   deployer.deploy(Pairing);
   deployer.link(BN256G2, Verifier);
   deployer.deploy(Verifier);
   deployer.link(BN256G2, Voting);
-  deployer.deploy(Register).then(() => {
-    deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'].map((candidate) => web3.utils.asciiToHex(candidate)), Register.address, { value: 1000000000000000, gas: 6700000});
-  });
+  deployer.deploy(Register)//.then(() => {
+  deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'].map((candidate) => web3.utils.asciiToHex(candidate)), Register.address, { value: 1000000000000000, gas: 6700000});
+  // });
 };
