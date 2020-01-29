b4b4a9e4375a5bd30ba624ae3d27d572b278841b
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-01-29 16:47:06 +0100

    finder returns table results with links

--- app/src/Finder.js
+++ app/src/Finder.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Web3 from "web3";
-import VotingArtifact from "./build/contracts/Voting.json";
+import Artifact from "./build/contracts/ElectionRegistry.json";
 
 import './App.css';
 
@@ -11,7 +11,6 @@ class Finder extends React.Component {
     super(props);
     this.state = { 
       status: '',
-      contracts: [],
     };
     if (window.ethereum) {
       // use MetaMask's provider
@@ -28,19 +27,16 @@ class Finder extends React.Component {
     try {
       // get contract instance
       const networkId = await this.web3.eth.net.getId();
-      const deployedNetwork = VotingArtifact.networks[networkId];
+      const deployedNetwork = Artifact.networks[networkId];
       console.log(`network: ${networkId}\ndeployedNetwork: ${deployedNetwork}`);
       this.meta = new this.web3.eth.Contract(
-        VotingArtifact.abi,
+        Artifact.abi,
         deployedNetwork.address,
       );
 
       // get accounts
       const accounts = await this.web3.eth.getAccounts();
       this.account = accounts[0];
-      this.getBudget();
-      await this.getCandidates();
-      this.getVotes();
 
     } catch (error) {
       console.error("Could not connect to contract or chain.");
@@ -48,39 +44,34 @@ class Finder extends React.Component {
     }
   }
 
-  async getCandidates() {
-    const { getCandidates } = this.meta.methods;
-    const candidateHexs = await getCandidates().call();
-    this.candidates = candidateHexs.map((candidate) => Web3.utils.hexToUtf8(candidate));
-  }
-
-  async showElections(results) {
-    const resultsTable = document.getElementById("resultsTable");
-    resultsTable.innerHTML = '';
-    let rowNum = 0;
-    for (const election of results) {
-      const row = resultsTable.insertRow(rowNum);
-      const nameCell = row.insertCell(0);
-      nameCell.innerHTML = election;
-      rowNum += 1;
+  showElections() {
+    if (this.state.searchResults) {
+      return this.state.searchResults.map((election) => {
+        const link = `/election/${election}`
+        return (<tr key={election}>
+          <td><a href={link}>{election}</a></td>
+        </tr>);
+      });
+    } else {
+        return (<></>);
     }
   }
 
   async search() {
 
-    const institution = document.getElementById("institution_finder").value;
+    const institution = document.getElementById("institutionFinder").value;
     if (institution) {
       this.setState({
         status: 'Your vote is being processed, have some patience',
       });
       const { findContract } = this.meta.methods;
-      const searchResults = await findContract(institution).call();
-      if(searchResults.length > 0) {
+      const searchResults = await findContract(institution).call()//.send({from: this.account});//.call();
+      if(searchResults && searchResults.length > 0) {
         this.setState({
-          status: 'Your vote has been processed',
-          contracts: searchResults,
+          status: '',
+          searchResults,
         });
-        this.showElections(searchResults);
+        this.showElections();
       } else {
         this.setState({ status: `No search results found for address: ${institution}`})
       }
@@ -96,12 +87,16 @@ class Finder extends React.Component {
         <p>{this.state.status}</p>
 
         <div className="container" id="actions">
-          <input type="text" id="institution_finder" placeholder="search for an institution" />
+          <input type="text" id="institutionFinder" placeholder="search for an institution" />
           <button onClick={this.search.bind(this)}>Search</button>
         </div>
 
         <div className="table-responsive">
-          <table id="resultsTable" className="table table-bordered"></table>
+          <table id="resultsTable" className="table table-bordered">
+            <tbody>
+              {this.showElections()}
+            </tbody>
+          </table>
         </div>
       </div>
     );
