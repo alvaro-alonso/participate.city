770a8b328ada1fbe3b0efe5bf6f65cf5f483d2f2
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-03-10 14:02:13 -0400

    migrations with one test election

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,8 +6,8 @@ const Register = artifacts.require('ElectionRegistry');
 
 const toHex = (candidates) => candidates.map((cand) => web3.utils.asciiToHex(cand));
 const voters = [
-  '92b34907ec85874ab6faadaa1fd32b3e86c1211c8fb9350a8bf13bd5caf1ff29',
-  '5d18f5dbfb0469a7136a3f3f95f46b3b1dbf434e1f9d95574ed8d40f8a40d0f1',
+  '0x' + '92b34907ec85874ab6faadaa1fd32b3e86c1211c8fb9350a8bf13bd5caf1ff29',
+  '0x' + '5d18f5dbfb0469a7136a3f3f95f46b3b1dbf434e1f9d95574ed8d40f8a40d0f1',
 ];
 const candidates2 = ['Vishnu', 'Jon', 'Maria'];
 const setup = { value: 1000000000000000, gas: 6700000 };
@@ -22,5 +22,6 @@ module.exports = async function(deployer) {
   await deployer.deploy(Register);
   const registerAdd = await Register.deployed();
 
-  await deployer.deploy(Voting, registerAdd.address, web3.utils.asciiToHex('treeRoot'), toHex(voters), toHex(candidates2), setup);
+  console.log(toHex(voters));
+  await deployer.deploy(Voting, registerAdd.address, web3.utils.asciiToHex('treeRoot'), voters, toHex(candidates2), setup);
 };
