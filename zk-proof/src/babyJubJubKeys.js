ac322e9024d05c2f688c9913c5ca01340cdf7653
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-04-10 23:29:05 +0200

    election root public input

--- zk-proof/src/babyJubJubKeys.js
+++ zk-proof/src/babyJubJubKeys.js
@@ -1,23 +1,26 @@
 const fs = require('fs');
 const { PublicKey, PrivateKey } = require('babyjubjub');
+const { MerkleTree } = require("merkletreejs");
+const ecc = require('eosjs-ecc');
+const Web3Utils = require('web3-utils');
  
 const voters = [];
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 56; i++) {
   //get PrivateKey object(field, hexstring)
   let sk = PrivateKey.getRandObj().field;
   //get PrivateKey object from field(or hexstring)
   let privKey = new PrivateKey(sk);
   //get PublicKey object from privateKey object
   let pubKey = PublicKey.fromPrivate(privKey);
+  const decToBNStr = (bn) => bn.toString().replace(".", "").replace(/([eE][-+]?[0-9]+).*$/, "")
+  const x = decToBNStr(pubKey.p.x.n), y = decToBNStr(pubKey.p.y.n);
+  const num = Buffer.from([...(Web3Utils.toBN(x).toArray()), ...(Web3Utils.toBN(y).toArray())]);
   const voter = {
-    privatekey: privKey.s.n.toString(),
-    point_x: pubKey.p.x.n.toString(),
-    point_y: pubKey.p.y.n.toString(),
+    privatekey: decToBNStr(privKey.s.n),
+    point_x: x,
+    point_y: y,
+    hash: ecc.sha256(num),
   };
-  console.log(voter);
-  voters.push(voter);
+  console.log(JSON.stringify(voter));
 }
-
-let data = JSON.stringify(voters);
-fs.writeFileSync('voters.json', data);
