c1299da100455d43729b38eebe8b6e160a5add01
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-09-29 22:18:32 +0200

    delete unneeded resources

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Finder from './Finder';
+import Home from './Home';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(<>
-  <Finder />
+  <Home />
 </>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
