fdaa8282713b2f0b420e03cfedd61bf63daa92b6
Alvaro Alonso Domenech <a.alonsodomenech@campus.tu-berlin.de>
2020-11-03 13:00:46 +0100

    removed helper functions from repository

--- zk-proof/src/merkleTree.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const { MerkleTree } = require("merkletreejs");
-const ecc = require('eosjs-ecc');
-const Web3Utils = require('web3-utils');
-
-const voters = JSON.parse(fs.readFileSync('./voters.json'));
-const hashes = voters.map(({ hash }) => hash);
-const tree = new MerkleTree(hashes, ecc.sha256);
-tree.print();
